c99ed316614013c72642c3f3dec1fda7f7f24217
Me <me@my-PDP11.local>
2020-04-26 02:09:27 +0100

    Add image preload to gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -3,10 +3,14 @@ function activateGallery() {
   let mainImage = document.querySelector("#gallery-photo img");
   let currentClass = "current";
   thumbnails.forEach((thumbnail) => {
+
+    let newImageSrc = thumbnail.dataset.largeVersion;
+    let largeVersion = new Image();
+    largeVersion.src = newImageSrc;
+
     thumbnail.addEventListener("click", function() {
 
       // Set clicked image as display image
-      let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
       let newImageAlt = thumbnail.dataset.title;
       mainImage.setAttribute("alt", newImageAlt);
